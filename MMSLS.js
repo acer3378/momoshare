e996f26c2843828e5092f14c1b66f0687387bb08
acer3378 <61084535+acer3378@users.noreply.github.com>
2024-02-21 12:00:56 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
             
-      "https://www.maimemo.com/share/page?uid=32600450&pid=77850b09aebb6d73de995df972ee2009&tid=829e95b4f2fa72e9177f4aaac3392eb6",
+    "https://www.maimemo.com/share/page?uid=32600450&pid=a0d775d72e19ac4bd20c67001b1126b6&tid=ced8f1b1968510d12d60b1d14f32e3f0",
 
     
 
